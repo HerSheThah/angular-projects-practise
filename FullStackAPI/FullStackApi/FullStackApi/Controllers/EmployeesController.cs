858aca98611a5d1ca472aab3e50fdaeed34a1d12
Admin <Admin@DESKTOP-BHPKJLM>
2022-10-13 14:58:00 +0530

    first angular projects

---
+++ FullStackAPI/FullStackApi/FullStackApi/Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using FullStackApi.Data;
+using FullStackApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+        public FullStackDbContext FullStackDbContext { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees= await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.id = Guid.NewGuid();
+            await _fullStackDbContext.Employees.AddAsync(employeeRequest);
+            _fullStackDbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ FullStackAPI/FullStackApi/FullStackApi/Data/FullStackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FullStackApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackApi.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions<FullStackDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+    
+}
